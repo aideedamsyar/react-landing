1cfe1961f05ca4535d2316bd204c98ec241deed6
Tengku Aideed Amsyar <aideedamsyar@gmail.com>
2021-12-11 14:01:12 +0800

    lastttttt commit

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef }  from 'react'
 import lottie from "lottie-web";
 import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate} from "react-router-dom";
-import { Button, ButtonGroup, Container, Heading, Link } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Container, Heading, Link } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 
 
@@ -21,13 +21,22 @@ function NotFound() {
     return (
         <div>
             <Container>
-            <Heading mt="2rem" lineHeight="2.5rem" fontSize="2.5rem">Hmm, the page doesn't seem to be available.</Heading>
-            <div className='containerrr' ref={container}></div>
-            <Text>If you think this is a mistake, feel free to <Link color="blue">send feedback</Link>.</Text>            
-            <ButtonGroup mt="1.5rem" mb="4rem">
-            <Button onClick={() => {navigate("/")}}>Back to home</Button>
-            </ButtonGroup>
+                <Heading mt="2rem" lineHeight="2.5rem" fontSize="2.5rem">Hmm, the page doesn't seem to be available.</Heading>
+                <div className='containerrr' ref={container}></div>            
+                <Text>If you think this is a mistake, feel free to send feedback.</Text> 
             </Container>
+            <Container>
+                <ButtonGroup mt="1.5rem" mb="4rem">
+                    <Button onClick={() => {navigate("/contact")}}>Send feedback</Button>
+                    <Button onClick={() => {navigate("/")}}>Back to home</Button>
+                </ButtonGroup>
+            </Container>
+            
+            
+            
+            
+
+
             
 
         </div>
