9e7308e9bf96dd0df41ed7410b20603cffec1f7a
Tengku Aideed Amsyar <82840182+aideedamsyar@users.noreply.github.com>
2024-02-03 18:51:13 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-CUSTOM_DOMAIN="https://work.aideedamsyar.com"
-# Build and deploy
+# Define variables
+CUSTOM_DOMAIN="work.aideedamsyar.com"
+REPO_URL="https://github.com/aideedamsyar/react-landing.git"
+GH_PAGES_BRANCH="gh-pages"
+
+# Build the project
 npm run build
-npm run deploy
 
-echo $CUSTOM_DOMAIN > CNAME
+# Navigate into the build directory
+cd build
 
-# Add changes to git
+# Create or update the CNAME file with your custom domain
+echo "$CUSTOM_DOMAIN" > CNAME
+
+# Initialize a new git repository in the build folder if it's not already a repo
+git init
+
+# Add remote repository
+git remote add origin $REPO_URL || git remote set-url origin $REPO_URL
+
+# Fetch the gh-pages branch
+git fetch origin $GH_PAGES_BRANCH
+
+# Checkout to the gh-pages branch or create it if it doesn't exist
+git checkout --orphan $GH_PAGES_BRANCH
+
+# Add all files to git
 git add .
 
-# Commit changes
-git commit -m "Deploying changes"
+# Commit the changes
+git commit -m "Deploying changes to GitHub Pages"
 
-# Push changes to main branch
-git push origin main
+# Force push to the gh-pages branch
+git push origin $GH_PAGES_BRANCH --force
+
+# Navigate back to the root project directory
+cd ..
